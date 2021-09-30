5ce02c2317f2731049165f221264d5b47b5eb289
Brad W <thatbr4d@gmail.com>
2021-09-30 13:24:32 -0400

    install and setup entity framework with postgres using migrations

--- DNR.Business/DataModels/TestModel.cs
+++ DNR.Business/DataModels/TestModel.cs
@@ -2,7 +2,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace business
+namespace DNR.Business.DataModels
 {
     [Table("Test")]
     public class TestModel
---
+++ DNR.Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using DNR.Business.DataModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DNR.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public virtual DbSet<TestModel> Tests { get; set; }
+    }
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory() + "/../DNR.Reactjsnet/appsettings.json").Build();
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("DatabaseConnection");
+            builder.UseNpgsql(connectionString);
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
