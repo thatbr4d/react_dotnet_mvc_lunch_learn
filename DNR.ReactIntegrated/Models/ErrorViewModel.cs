96a27ae82eae174d98c7f48730a6c9b49063ae32
thatbr4d <thatbr4d@gmail.com>
2021-11-15 15:49:37 -0500

    add new bare mvc project with launch config for integrated react

---
+++ DNR.ReactIntegrated/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DNR.ReactIntegrated.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
